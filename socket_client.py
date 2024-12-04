c8f70f1cbdd66252d2a197c95dd7836204d0426e
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2024-12-04 09:49:11 +0300

    Fix

--- socket_client.py
+++ socket_client.py
@@ -7,8 +7,9 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     s.connect(('localhost', 3030))
     print("Успешно подключено к серверу.")
-    s.sendall(b'Привет, сервер!')  # Отправляем сообщение
+    message = 'Привет, сервер!'  # Сообщение на русском
+    s.sendall(message.encode('utf-8'))  # Кодируем сообщение в байты UTF-8
     data = s.recv(1024)  # Получаем ответ от сервера
-    print(f"Ответ от сервера: {data.decode('utf-8')}")
+    print(f"Ответ от сервера: {data.decode('utf-8')}")  # Декодируем байты обратно в строку
 finally:
     s.close()  # Закрываем сокет
